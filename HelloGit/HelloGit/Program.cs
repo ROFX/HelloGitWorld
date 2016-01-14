45d0d40df51bdbe0e86e0fc1d48d9f36445ef3e0
ROFX <rogerio.felix@outlook.com>
2016-01-14 12:09:38 -0300

    No idea

---
+++ HelloGit/HelloGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Attempt at github with VS2015
+            Console.WriteLine("Hey there Git!");
+        }
+    }
+}
